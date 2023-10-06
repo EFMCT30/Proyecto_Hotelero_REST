37b6c8f89c608750219ec5e27a8c800033beaf37
Enzo Castillo Tara <94704797+EFMCT30@users.noreply.github.com>
2023-10-06 16:53:45 -0500

    Add files via upload

---
+++ Proyecto_Hotelero_REST/src/main/java/pe/edu/cibertec/Proyecto_Hotelero_REST/ProyectoHoteleroRestApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.Proyecto_Hotelero_REST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoHoteleroRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoHoteleroRestApplication.class, args);
+	}
+
+}
---
+++ Proyecto_Hotelero_REST/src/test/java/pe/edu/cibertec/Proyecto_Hotelero_REST/ProyectoHoteleroRestApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.Proyecto_Hotelero_REST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoHoteleroRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
